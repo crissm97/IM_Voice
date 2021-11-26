e04942b3477543eb14c4c6d143ed70b27a4c578e
Cristiano <jcristianosm@ua.pt>
2021-11-26 10:39:17 +0000

    já joga qualquer coisa

---
+++ Basis4Assignment2_newVersionApril2020/AppGui/AppGui/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using mmisharp;
+using Newtonsoft.Json;
+
+namespace AppGui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MmiCommunication mmiC;
+
+        //  new 16 april 2020
+        private MmiCommunication mmiSender;
+        private LifeCycleEvents lce;
+        private MmiCommunication mmic;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            mmiC = new MmiCommunication("localhost",8000, "User1", "GUI");
+            mmiC.Message += MmiC_Message;
+            mmiC.Start();
+
+            // NEW 16 april 2020
+            //init LifeCycleEvents..
+            lce = new LifeCycleEvents("APP", "TTS", "User1", "na", "command"); // LifeCycleEvents(string source, string target, string id, string medium, string mode
+            // MmiCommunication(string IMhost, int portIM, string UserOD, string thisModalityName)
+            mmic = new MmiCommunication("localhost", 8000, "User1", "GUI");
+            
+
+        }
+
+        private async void MmiC_Message(object sender, MmiEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+            var doc = XDocument.Parse(e.Message);
+            var com = doc.Descendants("command").FirstOrDefault().Value;
+            dynamic json = JsonConvert.DeserializeObject(com);
+            Console.WriteLine(json);
+            string move = "";
+            move += (string)json.recognized[1].ToString();
+            move += (string)json.recognized[2].ToString();
+            move += (string)json.recognized[4].ToString();
+            move += (string)json.recognized[5].ToString();
+            await Task.Run(() => MainAsync("WSEeAv9uljDu", move));
+
+            //  new 16 april 2020
+            await mmic.Send(lce.NewContextRequest());
+
+            string json2 = ""; // "{ \"synthesize\": [";
+            json2 += (string)json.recognized[0].ToString()+ " ";
+            json2 += (string)json.recognized[1].ToString() + " DONE." ;
+            //json2 += "] }";
+            /*
+             foreach (var resultSemantic in e.Result.Semantics)
+            {
+                json += "\"" + resultSemantic.Value.Value + "\", ";
+            }
+            json = json.Substring(0, json.Length - 2);
+            json += "] }";
+            */
+            var exNot = lce.ExtensionNotification(0 + "", 0 + "", 1, json2);
+            await mmic.Send(exNot);
+
+
+        }
+
+        
+        static async Task MainAsync(String Game, String Move)
+        {
+            var client = new HttpClient();
+            var url = new Uri("https://lichess.org/api/board/game/" + Game + "/move/" + Move);
+            var values = new Dictionary<string, string>()
+            {
+                {"", "" }
+            };
+            var content = new FormUrlEncodedContent(values);
+            client.DefaultRequestHeaders.Authorization
+                            = new AuthenticationHeaderValue("Bearer", "lip_WRQzhAeD2ZGNt1MZXsAI");
+            var result = await client.PostAsync(url, content);
+            string resultContent = await result.Content.ReadAsStringAsync();
+            Console.WriteLine(resultContent);
+        }
+        
+    }
+}
